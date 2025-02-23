a1ff76cd59c0eb22055d4d390521fc670f2bb2cd
ustinian-wang <wangjser@gmail.com>
2025-02-24 00:22:35 +0800

    fix: wrong export crypto

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 export * from "./src/utils/fs";
 export * from "./src/utils/http";
-export * from "./src/utils/cypto";
+export * from "./src/utils/crypto";
 
 export * from "./src/features/wcRobot";
